d66d2ffd8dd7e9f05260db436aeb7a10cd8581bd
Robert C. Duvall <robertcduvall@gmail.com>
2013-04-03 12:10:04 -0400

    Added main call

--- TagCloud.py
+++ TagCloud.py
@@ -6,3 +6,14 @@ def init (ignoreWords):
   myCommonWords = file.read().split()
   file.close()
 
+def countWords (input):
+  file = open(input, 'r')
+  for word in file.read().split():
+    word = removePuncuation(word.lower())
+    if isTaggableWord(word):
+      myWordCounts[word] = myWordCounts.get(word, 0) + 1
+  file.close()
+
+if __name__ == "__main__":
+  inputFile = sys.argv[1] if len(sys.argv) > 1 else 'speech.txt'
+  countWords(inputFile)
